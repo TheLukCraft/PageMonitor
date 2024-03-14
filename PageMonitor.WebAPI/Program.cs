44b31fc5063cd99fd4708bd13f066bf9658db8f7
TheLukCraft <luk1997r@o2.pl>
2024-03-14 10:46:19 +0100

    SeriLog - configuration

--- PageMonitor.WebAPI/Program.cs
+++ PageMonitor.WebAPI/Program.cs
@@ -1,11 +1,28 @@
+using Serilog;
+
 namespace PageMonitor.WebAPI
 {
     public class Program
     {
+        public static string APP_NAME = "PageMonitor.WebAPI";
+
         public static void Main(string[] args)
         {
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.WithProperty("Application", APP_NAME)
+                .Enrich.WithProperty("Application", Environment.MachineName)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateBootstrapLogger();
+
             var builder = WebApplication.CreateBuilder(args);
 
+            builder.Host.UseSerilog((context, services, configuration) => configuration
+            .Enrich.WithProperty("Application", APP_NAME)
+            .Enrich.WithProperty("Application", Environment.MachineName)
+            .ReadFrom.Configuration(context.Configuration)
+            .ReadFrom.Services(services)
+            .Enrich.FromLogContext());
             // Add services to the container.
 
             builder.Services.AddControllers();
@@ -15,7 +32,6 @@ namespace PageMonitor.WebAPI
             // Configure the HTTP request pipeline.
 
             app.UseAuthorization();
-
 
             app.MapControllers();
 
