6bafe8ad71e5b2192b27b6aac48a035f463ee180
TheLukCraft <luk1997r@o2.pl>
2024-03-15 01:04:49 +0100

    controller and account registration action

---
+++ PageMonitor.WebAPI/Controllers/BaseController.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PageMonitor.WebAPI.Controllers
+{
+    public abstract class BaseController : ControllerBase
+    {
+        protected readonly ILogger _logger;
+        protected readonly IMediator _mediator;
+
+        public BaseController(ILogger logger, IMediator mediator)
+        {
+            _logger = logger;
+            _mediator = mediator;
+        }
+    }
+}
---
+++ PageMonitor.WebAPI/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PageMonitor.Application.Logic.User;
+
+namespace PageMonitor.WebAPI.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class UserController : BaseController
+    {
+        public UserController(ILogger logger, IMediator mediator) : base(logger, mediator)
+        {
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateUserWithAccount([FromBody] CreateUserWithAccountCommand.Request model)
+        {
+            var createAccountResult = await _mediator.Send(model);
+            return Ok(createAccountResult);
+        }
+    }
+}
