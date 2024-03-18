26fae3bbcc4b60f6ce185fae3e6effc075f8b6b0
TheLukCraft <luk1997r@o2.pl>
2024-03-18 14:05:52 +0100

    Swagger

--- PageMonitor.WebAPI/Program.cs
+++ PageMonitor.WebAPI/Program.cs
@@ -49,8 +49,27 @@ namespace PageMonitor.WebAPI
             });
 
             builder.Services.AddApplicationServices();
+            builder.Services.AddSwaggerGen(o =>
+            {
+                o.CustomSchemaIds(x =>
+                {
+                    var name = x.FullName;
+                    if (name != null)
+                    {
+                        name = name.Replace("+", "_"); // swagger bug fix
+                    }
+
+                    return name;
+                });
+            });
 
             var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
 
             app.UseExceptionHandler();
 
